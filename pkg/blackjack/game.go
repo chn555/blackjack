54e49a221e5039239b0f8a28ee26d81e9a33d816
Chen Tondovski <chen.tondovski@onelayer.com>
2025-05-06 18:35:53 +0300

    blackjack: implement game logic
    Implemented most of the requirements for the game logic

---
+++ pkg/blackjack/game.go
@@ -0,0 +1,125 @@
+package blackjack
+
+import (
+	"context"
+	"fmt"
+
+	deckPb "github.com/chn555/schemas/proto/deck/v1"
+)
+
+// GameStatus represents the current state of the game
+type GameStatus uint8
+
+const (
+	UnknownState GameStatus = iota
+	WaitingToStart
+	WaitingForPlayer
+	Done
+)
+
+// Game represents the lifecycle of a blackjack game
+// It is not thread-safe and should be used in a single-threaded context
+type Game struct {
+	Status GameStatus
+
+	Players     map[string]*Player
+	playerOrder []string
+	NextPlayer  string
+	// Winner is empty if game is not Done
+	Winner string
+
+	deckClient deckPb.DeckServiceClient
+	DeckID     string
+}
+
+const DealerName = "Dealer"
+
+// NewGame creates a game, call Game.Start to start the game
+func NewGame(ctx context.Context, deckClient deckPb.DeckServiceClient) (*Game, error) {
+	if deckClient == nil {
+		return nil, fmt.Errorf("deck client is nil")
+	}
+
+	deckID, err := generateDeck(ctx, deckClient)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate deck: %w", err)
+	}
+
+	return &Game{
+		deckClient:  deckClient,
+		DeckID:      deckID,
+		Status:      WaitingToStart,
+		Players:     map[string]*Player{},
+		playerOrder: []string{},
+	}, nil
+}
+
+func generateDeck(ctx context.Context, deckClient deckPb.DeckServiceClient) (string, error) {
+	req := &deckPb.CreateDeckRequest{
+		Shuffle: true,
+	}
+
+	resp, err := deckClient.Create(ctx, req)
+	if err != nil {
+		return "", fmt.Errorf("failed to create deck: %w", err)
+	}
+
+	return resp.GetDeckId(), nil
+}
+
+// Start seeds the player hands and starts the game
+// The dealer is always the first player
+// To progress the game call Game.PlayTurn
+func (g *Game) Start(ctx context.Context, players ...string) error {
+	players = append([]string{DealerName}, players...)
+
+	err := g.createPlayers(ctx, players)
+	if err != nil {
+		return fmt.Errorf("failed to create players: %w", err)
+	}
+
+	g.Status = WaitingForPlayer
+	g.NextPlayer = g.playerOrder[0]
+
+	return g.playDealerHand(ctx)
+}
+
+func (g *Game) createPlayers(ctx context.Context, players []string) error {
+	for _, player := range players {
+		err := g.createPlayer(ctx, player)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (g *Game) createPlayer(ctx context.Context, player string) error {
+	p := &Player{}
+	var err error
+
+	p.Hand, err = g.newHandWithCards(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to create player hand: %w", err)
+	}
+
+	p.calculateScore()
+	g.Players[player] = p
+	g.playerOrder = append(g.playerOrder, player)
+	return nil
+}
+
+func (g *Game) newHandWithCards(ctx context.Context) (*Hand, error) {
+	hand, err := NewHand(g.DeckID, g.deckClient)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create hand: %w", err)
+	}
+
+	for range 2 {
+		if err := hand.PullCard(ctx); err != nil {
+			return nil, fmt.Errorf("failed to pull card: %w", err)
+		}
+	}
+
+	return hand, nil
+}
---
+++ pkg/blackjack/turn.go
@@ -0,0 +1,138 @@
+package blackjack
+
+import (
+	"context"
+	"fmt"
+)
+
+const blackjack = 21
+
+// TurnAction represents the action taken by the player during their turn
+type TurnAction uint8
+
+const (
+	Unknown TurnAction = iota
+	Hit
+	Stand
+)
+
+// Turn represents a player's turn in the game
+type Turn struct {
+	Action     TurnAction
+	PlayerName string
+}
+
+// NewTurn creates a new turn with the given action
+func NewTurn(action TurnAction, playerName string) *Turn {
+	return &Turn{
+		Action:     action,
+		PlayerName: playerName,
+	}
+}
+
+// PlayTurn processes the player's turn
+func (g *Game) PlayTurn(ctx context.Context, turn *Turn) error {
+	playerName := turn.PlayerName
+
+	if g.playerIsOutOfTurn(playerName) {
+		return fmt.Errorf("not your turn")
+	}
+
+	player, ok := g.Players[playerName]
+	if !ok {
+		return fmt.Errorf("player %s not found", playerName)
+	}
+
+	err := g.takeAction(ctx, turn, player)
+	if err != nil {
+		return fmt.Errorf("failed to take action: %w", err)
+	}
+
+	player.calculateScore()
+	g.Players[playerName] = player
+
+	if playerHitBlackjack(player) {
+		g.Status = Done
+		g.Winner = playerName
+		return nil
+	}
+
+	if playerWentOver(player) {
+		player.Bust = true
+		g.removePlayer(playerName)
+		g.Players[playerName] = player
+
+		if g.onlyOnePlayerLeft() {
+			g.Status = Done
+			g.Winner = g.playerOrder[0]
+			return nil
+		}
+	}
+
+	g.NextPlayer = g.getNextPlayer(playerName)
+	if g.NextPlayer == DealerName {
+		return g.playDealerHand(ctx)
+	}
+
+	return nil
+}
+
+func (g *Game) onlyOnePlayerLeft() bool {
+	return len(g.playerOrder) == 1
+}
+
+func playerWentOver(player *Player) bool {
+	return player.Score > blackjack
+}
+
+func playerHitBlackjack(player *Player) bool {
+	return player.Score == blackjack
+}
+
+func (g *Game) playerIsOutOfTurn(playerName string) bool {
+	return g.Status != WaitingForPlayer || g.NextPlayer != playerName
+}
+
+func (g *Game) takeAction(ctx context.Context, turn *Turn, player *Player) error {
+	switch turn.Action {
+	case Hit:
+		err := player.Hand.PullCard(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to draw card: %w", err)
+		}
+	case Stand:
+	default:
+		return fmt.Errorf("unknown turn: %v", turn)
+	}
+	return nil
+}
+
+// remove player
+func (g *Game) removePlayer(playerName string) {
+	for i, player := range g.playerOrder {
+		if player == playerName {
+			g.playerOrder = append(g.playerOrder[:i], g.playerOrder[i+1:]...)
+			break
+		}
+	}
+}
+
+func (g *Game) getNextPlayer(currentPlayer string) (playerName string) {
+	for i, player := range g.playerOrder {
+		if player == currentPlayer {
+			if i+1 < len(g.playerOrder) {
+				return g.playerOrder[i+1]
+			}
+			return g.playerOrder[0]
+		}
+	}
+	return ""
+}
+
+func (g *Game) playDealerHand(ctx context.Context) error {
+	return g.PlayTurn(ctx,
+		NewTurn(
+			Stand, DealerName,
+		),
+	)
+}
