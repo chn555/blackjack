1a8fd804b827c613af7f04847676872ae724f617
Chen Tondovski <chen.tondovski@onelayer.com>
2025-05-07 16:24:45 +0300

    cmd/blackjack: added aiClient dependency for game service

--- cmd/blackjack/main.go
+++ cmd/blackjack/main.go
@@ -8,6 +8,7 @@ import (
 
 	blackjackImpl "github.com/chn555/blackjack/internal/blackjack"
 	"github.com/chn555/blackjack/pkg/blackjack"
+	aiPb "github.com/chn555/schemas/proto/ai/v1"
 	blackjackPb "github.com/chn555/schemas/proto/blackjack/v1"
 	deckPb "github.com/chn555/schemas/proto/deck/v1"
 	"google.golang.org/grpc"
@@ -28,9 +29,16 @@ func main() {
 	defer conn.Close()
 
 	deckClient := deckPb.NewDeckServiceClient(conn)
-
+	// Create a connection to the deck service
+	aiConn, err := grpc.NewClient("localhost:8082", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		slog.Error("Error connecting to deck service", err)
+		os.Exit(1)
+	}
+	defer aiConn.Close()
+	aiClient := aiPb.NewAiServiceClient(aiConn)
 	// Create the blackjack server with the deck client
-	blackjackServer, err := blackjackImpl.NewServiceServer(store, deckClient)
+	blackjackServer, err := blackjackImpl.NewServiceServer(store, deckClient, aiClient)
 	if err != nil {
 		slog.Error("Error creating blackjack server", err)
 		os.Exit(1)
