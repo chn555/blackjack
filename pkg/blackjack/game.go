8d4b91568906e6027d0282b3e52a79cc1fa08efe
Chen Tondovski <chen.tondovski@onelayer.com>
2025-05-08 11:09:38 +0300

    blackjack: pull cards from deck service
    We use the deck gRPC service to create a deck for the game and pull cards to the players hands.
    At the start of a game, 2 cards are pull into the hand of each player.

--- pkg/blackjack/game.go
+++ pkg/blackjack/game.go
@@ -1,8 +1,10 @@
 package blackjack
 
 import (
+	"context"
 	"fmt"
 
+	deckPb "github.com/chn555/schemas/proto/deck/v1"
 	"github.com/google/uuid"
 )
 
@@ -27,27 +29,54 @@ type Game struct {
 	NextPlayer  string
 	// Winner is empty if game is not Done
 	Winner string
+
+	deckClient deckPb.DeckServiceClient
+	DeckID     string
 }
 
 const DealerName = "Dealer"
 
 // NewGame creates a game, call Game.Start to start the game
-func NewGame() *Game {
+func NewGame(ctx context.Context, deckClient deckPb.DeckServiceClient) (*Game, error) {
+	if deckClient == nil {
+		return nil, fmt.Errorf("deck client is nil")
+	}
+
+	deckID, err := generateDeck(ctx, deckClient)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate deck: %w", err)
+	}
+
 	return &Game{
 		ID:          uuid.New().String(),
+		deckClient:  deckClient,
+		DeckID:      deckID,
 		Status:      WaitingToStart,
 		Players:     map[string]*Player{},
 		playerOrder: []string{},
+	}, nil
+}
+
+func generateDeck(ctx context.Context, deckClient deckPb.DeckServiceClient) (string, error) {
+	req := &deckPb.CreateDeckRequest{
+		Shuffle: true,
 	}
+
+	resp, err := deckClient.Create(ctx, req)
+	if err != nil {
+		return "", fmt.Errorf("failed to create deck: %w", err)
+	}
+
+	return resp.GetDeckId(), nil
 }
 
 // Start seeds the player hands and starts the game
 // The dealer is always the first player
 // To progress the game call Game.PlayTurn
-func (g *Game) Start(players ...string) error {
+func (g *Game) Start(ctx context.Context, players ...string) error {
 	players = append([]string{DealerName}, players...)
 
-	err := g.createPlayers(players)
+	err := g.createPlayers(ctx, players)
 	if err != nil {
 		return fmt.Errorf("failed to create players: %w", err)
 	}
@@ -58,9 +87,9 @@ func (g *Game) Start(players ...string) error {
 	return nil
 }
 
-func (g *Game) createPlayers(players []string) error {
+func (g *Game) createPlayers(ctx context.Context, players []string) error {
 	for _, player := range players {
-		err := g.createPlayer(player)
+		err := g.createPlayer(ctx, player)
 		if err != nil {
 			return err
 		}
@@ -68,11 +97,11 @@ func (g *Game) createPlayers(players []string) error {
 	return nil
 }
 
-func (g *Game) createPlayer(player string) error {
+func (g *Game) createPlayer(ctx context.Context, player string) error {
 	p := &Player{}
 	var err error
 
-	p.Hand, err = g.newHandWithCards()
+	p.Hand, err = g.newHandWithCards(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to create player hand: %w", err)
 	}
@@ -83,10 +112,16 @@ func (g *Game) createPlayer(player string) error {
 	return nil
 }
 
-func (g *Game) newHandWithCards() (*Hand, error) {
-	hand, err := NewHand()
+func (g *Game) newHandWithCards(ctx context.Context) (*Hand, error) {
+	hand, err := NewHand(g.DeckID, g.deckClient)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create hand: %w", err)
+	}
+
+	for range 2 {
+		if err := hand.PullCard(ctx); err != nil {
+			return nil, fmt.Errorf("failed to pull card: %w", err)
+		}
 	}
 
 	return hand, nil
