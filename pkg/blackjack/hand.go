bddfdf7c71a4347543003eb33378048dae86be3f
Chen Tondovski <chen.tondovski@onelayer.com>
2025-05-12 12:26:38 +0300

    blackjack: score a player's hand
    In this commit we introduce cards to hands, and create a function that scores a player by the cards in their hand.

--- pkg/blackjack/hand.go
+++ pkg/blackjack/hand.go
@@ -1,4 +1,16 @@
 package blackjack
 
+import (
+	"github.com/chn555/schemas/proto/deck/v1"
+)
+
 // Hand represents a player's hand in the game, consisting of the player's cards.
-type Hand struct{}
+type Hand struct {
+	// Cards represents the cards in the player's hand.
+	Cards []*v1.Card
+}
+
+// NewHand creates a new empty hand.
+func NewHand() (*Hand, error) {
+	return &Hand{}, nil
+}
--- pkg/blackjack/player.go
+++ pkg/blackjack/player.go
@@ -9,3 +9,20 @@ type Player struct {
 	// Bust indicates whether the player has gone bust (over 21).
 	Bust bool
 }
+
+func (p *Player) calculateScore() {
+	points := 0
+
+	for _, card := range p.Hand.Cards {
+		value := int(card.GetValue())
+		if value == 1 && points+11 <= 21 {
+			value = 11 // Ace can be 1 or 11, prefer 11 if it doesn't bust
+		}
+		if value > 10 {
+			value = 10 // Face cards are worth 10
+		}
+		points += value
+	}
+
+	p.Score = points
+}
